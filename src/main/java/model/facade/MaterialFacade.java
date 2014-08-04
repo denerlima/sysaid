ed1d1b12c3d4ff06b72c2c0945bdb8771582ec19
Denner <kardok@gmail.com>
2014-08-04 00:40:47 -0300

    Comentarios AtualizaEstoqueCalculado

--- src/main/java/model/facade/MaterialFacade.java
+++ src/main/java/model/facade/MaterialFacade.java
@@ -1,6 +1,7 @@
 package model.facade;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -36,6 +37,18 @@ public class MaterialFacade extends GenericFacade<Material> implements Serializa
 	public List<Material> pesquisarListaMateriaisbyNomeMaterial(String nomeMaterial) {		
 		List<Material> result = materialDAO.findListMaterialByMaterial(nomeMaterial);        
 		return result;
+    }
+	
+	public void atualizaEstoqueMinimoCalculado(Material material , Integer qtdeMeses) throws Exception {
+		BigDecimal mediaMensal = new BigDecimal(30);		
+		// Será calculada a média mensal de todas as SAÍDAS de estoque, descontando as DEVOLUÇÕES;
+		// @qtdeMeses Quantidade de meses de movimentação imediatamente anteriores ao mês atual a serem considerados no cálculo do estoque mínimo; 
+		// Verificar o SUM da Coluna
+		// mediaMensal = materialDAO.findMediaMaterial(material , qtdeMeses);
+		
+		//Adicionar uma dataAtual na MF_ORDEMSERVICO_MF_MATERIAL
+		material.setEstoqueCalculado(mediaMensal);		
+		update(material);
     }
 	
 	@Override
